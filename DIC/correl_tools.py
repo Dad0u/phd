d11f43b7e863c92ee9d03edcc847ed498c9919f7
Victor Couty <victor@couty.eu>
2020-06-26 15:14:04 +0200

    [fix] Fixed resample2

--- DIC/correl_tools.py
+++ DIC/correl_tools.py
@@ -21,22 +21,21 @@ def resample2(im,order=1):
   """
   Stupid resampling to divide resolution by 2
   """
-  assert isinstance(order,int) and order >= 1,"Order must be an int >=1"
+  assert isinstance(order,int) and order >= 0,"Order must be an int >=1"
+  if order == 0:
+    return im
+  if 1 in im.shape:
+    raise RuntimeError("Cannot resample image, dim < 1")
+  imf = im.astype(np.float)
   y,x = im.shape
   if y%2:
-    im = im[:-1,:]
+    imf = imf[:-1,:]
     print("Warning, dropped pixels on axis 0 to resample!")
   if x%2:
-    im = im[:,:-1]
+    imf = imf[:,:-1]
     print("Warning, dropped pixels on axis 1 to resample!")
   if order == 1:
-    return im[::2,::2]+im[1::2,::2]+im[::2,1::2]+im[1::2,1::2]
+    return ((imf[::2,::2]+imf[1::2,::2]+imf[::2,1::2]+imf[1::2,1::2])/4
+            ).astype(im.dtype)
   else:
     return resample2(resample2(im,order-1))
-
-# float sigma=.6f
-# int fpi=5
-# float alpha = 1.f
-# float delta=.5f
-# float gamma=5.f
-# float downscaleFactor = .95f
