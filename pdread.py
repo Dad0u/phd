a1213bff98c82cb8ede23eb9a39e71800fe0597b
Victor Couty <victor@couty.eu>
2020-07-01 16:24:03 +0200

    [fix] Finished folder detection in pdread

--- pdread.py
+++ pdread.py
@@ -16,22 +16,19 @@ paths = sorted(glob("data/*/"))
 cuts = []
 for p in paths:
   try:
-    cuts.append(np.loadtxt(p+'cut.txt'))
+    cuts.append(float(np.loadtxt(p+'cut.txt')))
   except OSError:
     cuts.append(None)
 
 # Making a list of test containing the split times
-test = []
+tests = []
 new = True
 for p,c in zip(paths,cuts):
   if new:
-    test.append([(p,c)])
+    tests.append([(p,c)])
   else:
-    test[-1].append((p,c))
+    tests[-1].append((p,c))
   new = c is None
-
-
-print(f"Found {len(test)} different tests")
 
 
 def read_single(path):
@@ -42,6 +39,7 @@ def read_single(path):
   At every timestamp, the unknown values are interpolated.
   The result is NOT resampled (timestamps are those from both the csv files)
   """
+  print("[read_single] Called with",path)
   lj = pd.read_csv(path+'lj.csv.gz', delimiter=',\\s+',engine='python')
   lj['t(s)'] = pd.to_timedelta(lj['t(s)'], unit='s')
   lj = lj.set_index('t(s)')
@@ -56,7 +54,7 @@ def read_single(path):
   return data_high
 
 
-def read(freq='10ms',paths=paths[0],cuts=cuts):
+def read_test(test,freq='10ms'):
   """
   Reads several protions of a test and builds a virtual "continuous" test
   Will resample the dataframe to space evenly the timestamps
@@ -64,20 +62,32 @@ def read(freq='10ms',paths=paths[0],cuts=cuts):
   Takes the folders and the timestamps where each portion of the test ends
   (except for the last one)
   """
+  print("[read] Called with",test)
   # Read all the tests
-  frames = [read_single(p) for p in paths]
+  frames = [read_single(path) for path,cut in test]
   # Cut them at the proper length
-  frames = [frame[frame.index<pd.Timedelta(cut,'s')]
-      for frame,cut in zip(frames,cuts)] + [frames[-1]]
+  cuts = [cut for path,cut in test]
+  frames = [frame[frame.index<pd.Timedelta(cut,'s')] if cut else frame
+      for frame,cut in zip(frames,cuts)]
   # Offset the timestamps accordingly
-  for frame,cut in zip(frames[1:],np.cumsum(cuts)):
+  for frame,cut in zip(frames[1:],np.cumsum(cuts[:-1])):
     frame.index += pd.Timedelta(cut,'s')
   # Concatenate and resample
   return pd.concat(frames).resample(freq).mean()
 
 
+def read_all(tests=tests,freq='10ms'):
+  """
+  Reads all the tests in the folder
+  """
+  return [read_test(t,freq) for t in tests]
+
+
 if __name__ == '__main__':
-  frame = read()
+  print(f"Found {len(tests)} different tests")
+  for i,t in enumerate(tests):
+    print(i+1,[i[0] for i in t])
+  frame = read_test(tests[0])
   frame.plot(y='exx(%)')
   frame.plot('exx(%)','F(N)')
   plt.show()
