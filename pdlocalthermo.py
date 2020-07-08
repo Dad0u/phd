868bdd090ebc86532619e7b68cf1ba0558dae4ab
Victor Couty <victor@couty.eu>
2020-07-08 16:37:38 +0200

    [add] pdlocalthermo

---
+++ pdlocalthermo.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+from glob import glob
+import cv2
+from mystuff.cachedfunc import cachedfunc
+
+CUTFILE = 'cut.txt'
+MASKFILE = 'irmask.tif'
+IMGPATH = 'IR/*.tif'
+XIPATH = 'imagesXiD-face/*.tiff'
+IRTHRESH = 'irthresh.txt' # Value of a pixel (diff of DL) to be considered hot
+# Not to be confused with the number of hot pixel threshold (see pdmulti)
+
+
+class TimeGetter:
+  """
+  To retreive the timestamp of an image given its index
+
+  Uses the Ximea images (the cameras were in sync)
+  """
+  def __init__(self,path):
+    xilist = sorted(glob(path+XIPATH))
+    self.tlist = [float(s.split('_')[-1][:-5]) for s in xilist]
+
+  def get(self,i):
+    if isinstance(i,str):
+      i = int(i.split('_')[-1][:-4])
+    return self.tlist[i]
+
+
+@cachedfunc('localthermo.p')
+def read_localthermo(paths):
+  """
+  To compute a damage evolution based on local temperature elevations
+  """
+  total_offset = 0
+  frames = []
+  for path in paths:
+    try:
+      cut = np.loadtxt(path+CUTFILE)
+    except OSError:
+      cut = np.inf
+    imglist = sorted(glob(path+IMGPATH),
+        key=lambda s:int(s.split('_')[-1][:-4]))
+    last = cv2.imread(imglist[0],cv2.IMREAD_ANYDEPTH)
+    h,w = last.shape
+    try:
+      mask = cv2.imread(path+MASKFILE,0).astype(float)/255
+    except (OSError,AttributeError):
+      print("[read_localthermo] Warning, mask not found! Using default")
+      margin = .2 # 20% margin on the default mask
+      mask = np.zeros((h,w))
+      mask[int(margin*h):int((1-margin)*h),int(margin*w):int((1-margin)*w)] = 1
+    tg = TimeGetter(path)
+    try:
+      irthresh = int(np.loadtxt(IRTHRESH))
+    except OSError:
+      print(f"[warning] {IRTHRESH} not found, using default value")
+      irthresh = 50
+    r = []
+    for imgname in imglist[1:]:
+      t = tg.get(imgname)
+      if t>= cut:
+        break
+      img = cv2.imread(imgname,cv2.IMREAD_ANYDEPTH).astype(float)
+      diff = img-last
+      #r.append((t,np.sum(mask*diff**2)))
+      mdiff = mask*(diff-diff[np.where(mask)].mean())
+      r.append((t+total_offset,np.count_nonzero(mdiff>irthresh)))
+
+    total_offset += min(cut,t)
+    data = pd.DataFrame(r,columns=['t(s)','localthermo'])
+    data['t(s)'] = pd.to_timedelta(data['t(s)'],unit='s')
+    frames.append(data.set_index('t(s)'))
+  return pd.concat(frames)
--- pdmulti.py
+++ pdmulti.py
@@ -1,23 +1,40 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from phd import pdread,pdaudio
+from phd import pdread,pdaudio,pdlocalstrain,pdlocalthermo
 
-THRESH_FILE = 'audio_thresh.txt'
+THRESH_FILE = 'thresh.txt'
+default_thresh = {'audio':50,'localstrain':40,'localthermo':30}
+
 try:
-  thresh = float(np.loadtxt(THRESH_FILE))
-except OSError:
-  thresh = 50
-  print(f"Warning, {THRESH_FILE} not found. Using default value {thresh}")
+  with open(THRESH_FILE,'r') as f:
+    thresh = eval(f.read())
+  assert isinstance(thresh,dict)
+except Exception:
+  print(f"[pdmulti] WARN {THRESH_FILE} not found. Using default values")
+  thresh = default_thresh
+for k in default_thresh:
+  if k not in thresh:
+    print(f"[pdmulti] WARN {THRESH_FILE} does not contain {k}. Using default")
+    thresh[k] = default_thresh[k]
 
 tests = pdread.read_all()
 
 audios = [pdaudio.read_audio(test) for test in pdread.tests]
 
-for t,a in zip(tests,audios):
-  data = pd.concat([t,a]).interpolate('time').resample('10ms').mean()
+lstrain = [pdlocalstrain.read_localstrain(test) for test in pdread.tests]
+
+lthermo = [pdlocalthermo.read_localthermo(test) for test in pdread.tests]
+
+for t,a,ls,lt in zip(tests,audios,lstrain,lthermo):
+  data = pd.concat([t,a,ls,lt]).interpolate('time').resample('10ms').mean()
   t = np.array(data.index,float)/1e9
-  plt.plot(t,data['exx(%)'])
-  audiodmg = data['audio_lvl'] > thresh
-  plt.plot(t[audiodmg],data['exx(%)'][audiodmg],'o')
+  plt.plot(t,data['exx(%)'],label='$\\epsilon_{xx}$(%)')
+  audiodmg = data['audio_lvl'] > thresh['audio']
+  plt.plot(t[audiodmg],data['exx(%)'][audiodmg],'o',label='audio')
+  lstraindmg = data['localstrain'] > thresh['localstrain']
+  plt.plot(t[lstraindmg],data['exx(%)'][lstraindmg]+.01,'o',label='correl')
+  lthermodmg = data['localthermo'] > thresh['localthermo']
+  plt.plot(t[lthermodmg],data['exx(%)'][lthermodmg]+.02,'o',label='thermo')
+  plt.legend()
 plt.show()
