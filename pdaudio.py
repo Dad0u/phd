275b9610f28d179e1caa97c79406c6a896d11a25
Victor Couty <victor@couty.eu>
2021-05-27 15:39:31 +0200

    Changed syntax in pdaudio for PEP8

--- pdaudio.py
+++ pdaudio.py
@@ -6,7 +6,7 @@ from mystuff.cachedfunc import cachedfunc
 WAVFILE = 'audio_denoised.wav'
 OFFSETFILE = 'offset_audio.txt'
 CUTFILE = 'cut.txt'
-SAMPLERATE = 192000 # kHz, samplerate of the wav file
+SAMPLERATE = 192000  # kHz, samplerate of the wav file
 
 
 @cachedfunc('audio.p')
@@ -22,24 +22,24 @@ def read_audio(paths, freq='10ms', samplerate=SAMPLERATE):
   total_offset = 0
   frames = []
   for p in paths:
-    offset = np.loadtxt(p+OFFSETFILE)
+    offset = np.loadtxt(p + OFFSETFILE)
     try:
-      cut = np.loadtxt(p+CUTFILE)
+      cut = np.loadtxt(p + CUTFILE)
     except OSError:
       cut = None
-    f,wav = wavfile.read(p+WAVFILE)
-    wav = wav[int(offset*samplerate):cut and int((cut+offset)*samplerate)]
-    dwav = pd.DataFrame(np.abs(wav),columns=['audio_lvl'])
-    t = np.arange(0,len(dwav))/SAMPLERATE
+    f, w = wavfile.read(p + WAVFILE)
+    wav = w[int(offset * samplerate):cut and int((cut + offset) * samplerate)]
+    dwav = pd.DataFrame(np.abs(wav), columns=['audio_lvl'])
+    t = np.arange(0, len(dwav)) / SAMPLERATE
     t += total_offset
     total_offset += cut if cut else 0
-    dwav.index = pd.to_timedelta(t,unit='s')
+    dwav.index = pd.to_timedelta(t, unit='s')
     dwav.index.name = 't(s)'
     frames.append(dwav.resample(freq).mean())
   return pd.concat(frames)
 
 
-def count_evt(frame,thresh=100):
+def count_evt(frame, thresh=100):
   ser = frame['audio_lvl']
-  d = (ser>thresh).astype(int)
-  return (d.diff()==1).cumsum()
+  d = (ser > thresh).astype(int)
+  return (d.diff() == 1).cumsum()
