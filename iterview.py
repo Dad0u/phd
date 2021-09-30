66b3d304d14f3cfa5456a900ac3b8033b8e7968f
Victor Couty <victor@couty.eu>
2021-09-30 17:32:09 +0200

    [add] Added option to generate a video in iterview

--- iterview.py
+++ iterview.py
@@ -9,7 +9,11 @@ from time import time, sleep
 matplotlib.use('TKAgg')
 
 
-class Displayer(object):
+def normalize(img, lo, hi):
+  return (np.clip(img, lo, hi).astype(float) - lo) / (hi - lo)
+
+
+class Displayer:
   """
     imgs: list of indexes/names
     clim: Scale to use
@@ -27,6 +31,8 @@ class Displayer(object):
     self.last_v = -1
     self.cimg = {}
     self.cclim = {}
+
+  def init_window(self):
     self.root = tk.Tk()
     self.slider = tk.Scale(self.root, orient='horizontal',
                            from_=1, to=len(self.keys), length=500)
@@ -44,7 +50,8 @@ class Displayer(object):
       return a, b
     except TypeError:
       # Then it should be a float/int, let's compute the percentiles
-      return np.percentile(img, self.scale), np.percentile(img, 100 - self.scale)
+      return (np.percentile(img, self.scale),
+              np.percentile(img, 100 - self.scale))
 
   def show_interactive(self, event=None):
     v = self.slider.get() - 1
@@ -54,7 +61,7 @@ class Displayer(object):
       self.last_v = v
     self.root.after(250, self.show_interactive)
 
-  def show(self, key):
+  def get_img(self, key):
     if key not in self.cimg:
       img = self.get(key)
       if self.cache:
@@ -66,7 +73,10 @@ class Displayer(object):
     else:
       lo, hi = self.get_scale(img)
       self.cclim[key] = lo, hi
+    return (img, lo, hi)
 
+  def show(self, key):
+    img, lo, hi = self.get_img(key)
     self.key_label.configure(text=str(key))
     self.scale_label.configure(text="[%f, %f]" % (lo, hi))
     if not hasattr(self, 'im'):
@@ -81,6 +91,7 @@ class Displayer(object):
     plt.pause(.1)
 
   def interactive(self):
+    self.init_window()
     self.root.after(250, self.show_interactive)
     self.root.mainloop()
     self.end()
@@ -89,6 +100,7 @@ class Displayer(object):
     plt.close('all')
 
   def animate(self, freq=10):
+    self.init_window()
     t1 = time()
     for i, key in enumerate(self.keys):
       self.show(key)
@@ -99,11 +111,27 @@ class Displayer(object):
       if delay > 0:
         sleep(delay)
 
+  def make_video(self, vidname, fps=30.):
+    import cv2
+    cm = getattr(matplotlib.cm, self.cmap)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    img, *_ = self.get_img(self.keys[0])
+    vid = cv2.VideoWriter(vidname, fourcc, fps, (img.shape[::-1]))
+    try:
+      for i, k in enumerate(self.keys):
+        print(f"{i}/{len(self.keys)}")
+        img, lo, hi = self.get_img(k)
+        img_c = (cm(normalize(img, lo, hi)) * 255).astype(np.uint8)
+        vid.write(img_c[:, :, -2::-1])
+    finally:
+      vid.release()
+
 
 if __name__ == '__main__':
-  img = (plt.imread('/home/vic/test/lena.png') * 255).astype(np.uint8)
+  img = (plt.imread('/home/vic/Images/speckle.png') * 255).astype(np.uint8)
 
   d = Displayer(range(255), lambda i: img + i, scale=(100, 200), cache=False)
 
   d.interactive()
   # d.animate()
+  # d.make_video('test.mp4')
