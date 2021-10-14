7d837eaa6638f907f941f66e3ba1d743057510d4
Victor Couty <victor@couty.eu>
2021-10-14 18:34:30 +0200

    [add] Async loading in Displayer.make_video

--- iterview.py
+++ iterview.py
@@ -5,12 +5,54 @@ import matplotlib
 import matplotlib.pyplot as plt
 import tkinter as tk
 from time import time, sleep
+from multiprocessing import Process, Queue
 
 matplotlib.use('TKAgg')
 
 
 def normalize(img, lo, hi):
   return (np.clip(img, lo, hi).astype(float) - lo) / (hi - lo)
+
+
+class Async_iter(Process):
+  """
+  Can be used to iterate over a list of objects and apply
+  asynchronously a function (like an async map)
+
+  For example to load images to be processed in an other process
+
+  namelist: List of keys
+
+  load: function to call on each key
+
+  length: The number of element to load in advance
+
+  sleep_delay: How long will the process sleep if there no elements are needed
+  """
+  def __init__(self, namelist, load, length=3, sleep_delay=.1):
+    super().__init__()
+    self.namelist = namelist
+    self.load = load
+    self.q = Queue()
+    self.length = length
+    self.sleep_delay = sleep_delay
+
+  def __iter__(self):
+    self.start()
+    return self
+
+  def run(self):
+    for name in self.namelist:
+      while self.q.qsize() >= self.length:
+        sleep(self.sleep_delay)
+      self.q.put(self.load(name))
+
+  def __next__(self):
+    while self.q.qsize() == 0 and self.is_alive():
+      sleep(self.sleep_delay)
+    if self.q.qsize() > 0:
+      return self.q.get()
+    raise StopIteration
 
 
 class Displayer:
@@ -111,27 +153,31 @@ class Displayer:
       if delay > 0:
         sleep(delay)
 
+  def mkimg_color(self, key):
+    cm = getattr(matplotlib.cm, self.cmap)
+    img, lo, hi = self.get_img(key)
+    return (cm(normalize(img, lo, hi)) * 255).astype(np.uint8)[:, :, -2::-1]
+
   def make_video(self, vidname, fps=30.):
     import cv2
-    cm = getattr(matplotlib.cm, self.cmap)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     img, *_ = self.get_img(self.keys[0])
     vid = cv2.VideoWriter(vidname, fourcc, fps, (img.shape[::-1]))
+    ai = Async_iter(self.keys, self.mkimg_color)
     try:
-      for i, k in enumerate(self.keys):
+      for i, img in enumerate(ai):
         print(f"{i}/{len(self.keys)}")
-        img, lo, hi = self.get_img(k)
-        img_c = (cm(normalize(img, lo, hi)) * 255).astype(np.uint8)
-        vid.write(img_c[:, :, -2::-1])
+        vid.write(img)
     finally:
       vid.release()
+      ai.terminate()
 
 
 if __name__ == '__main__':
   img = (plt.imread('/home/vic/Images/speckle.png') * 255).astype(np.uint8)
 
-  d = Displayer(range(255), lambda i: img + i, scale=(100, 200), cache=False)
+  d = Displayer(range(0, 255, 3), lambda i: img + i, scale=(100, 200), cache=False)
 
-  d.interactive()
+  #d.interactive()
   # d.animate()
-  # d.make_video('test.mp4')
+  d.make_video('test.mp4')
